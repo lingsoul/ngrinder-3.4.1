a68a779e782013b4225e6d6336c8a75335d13cf2
geunwoo son <songeunwoo@nhn.com>
2016-10-19 19:28:27 +0900

    Cubrid version upgrade  - as-is 8.4.1 to-be 9.2.26  - modify pom.xml  - modify Bigint ColumnType

--- ngrinder-controller/src/main/java/org/hibernate/dialect/CUBRIDExDialect.java
+++ ngrinder-controller/src/main/java/org/hibernate/dialect/CUBRIDExDialect.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
@@ -9,7 +9,7 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License. 
+ * limitations under the License.
  */
 package org.hibernate.dialect;
 
@@ -47,7 +47,7 @@ public class CUBRIDExDialect extends Dialect {
 	public CUBRIDExDialect() {
 		super();
 		registerColumnType(Types.BIT, "bit(8)");
-		registerColumnType(Types.BIGINT, "decimal");
+		registerColumnType(Types.BIGINT, "numeric");
 		registerColumnType(Types.BOOLEAN, "char(2)");
 		registerColumnType(Types.SMALLINT, "smallint");
 		registerColumnType(Types.TINYINT, "smallint");
