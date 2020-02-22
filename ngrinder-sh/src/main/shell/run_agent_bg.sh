2bd135a429634813a9c9c67917cdb774389ba7ee
lingjian <lingjian@qudian.com>
2020-02-22 15:11:48 +0800

    修改run_agent.sh

--- ngrinder-sh/src/main/shell/run_agent_bg.sh
+++ ngrinder-sh/src/main/shell/run_agent_bg.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 curpath=`pwd |awk -F '/' '{print $NF}'`
+owned_user=`pwd |awk -F '/' '{print $NF}'|awk -F '-' '{print $NF}'`
+if [ "$curpath" != "ngrinder-agent" ];then
+    sed -i "s/.*agent.region=.*/agent.region=`echo NONE_owned_$owned_user`/" __agent.conf
+    echo agent.region=$owned_user
+fi
 nohup ./run_agent.sh -o  -ah ~/.${curpath} --host-id ${curpath} $@ > /dev/null & 2>&1
--- ngrinder-sh/src/main/shell/run_monitor.sh
+++ ngrinder-sh/src/main/shell/run_monitor.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 curpath=`dirname $0`
 cd ${curpath}
-local_ip=`/sbin/ifconfig|grep Bcast|head -n 1|awk '{print $2}'|awk -F ":" '{print $2}'`
-sed -i "3c monitor.binding_ip=`echo $local_ip`" __agent.conf
+os=`uname -o`
+if ( echo ${os} |grep -q "inux" );then
+    local_ip=`/sbin/ifconfig eth0|grep 'inet addr' |awk '{print $2}'|awk -F ":" '{print $2}'`
+    sed -i "s/.*monitor.binding_ip=.*/monitor.binding_ip=`echo $local_ip`/" __agent.conf
+    echo replace ip: $local_ip
+fi
 java -server -cp "lib/*" org.ngrinder.NGrinderAgentStarter --mode monitor --command run $@
