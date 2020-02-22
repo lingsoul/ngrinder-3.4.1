b6d1994cc7fda6c5cca6cf1c5ed951a896b68e94
lingjian <lingjian@qudian.com>
2020-02-22 16:49:37 +0800

    修改run_monitor.sh

--- ngrinder-sh/src/main/shell/run_monitor.sh
+++ ngrinder-sh/src/main/shell/run_monitor.sh
@@ -1,16 +1,4 @@
 #!/bin/sh
-curpath=`pwd`
+curpath=`dirname $0`
 cd ${curpath}
-os=`uname -a`
-
-if ( echo ${os} |grep -q "inux" );then
-    local_ip=`/sbin/ifconfig eth0|grep 'inet addr' |awk '{print $2}'|awk -F ":" '{print $2}'`
-    sed -i "s/.*monitor.binding_ip=.*/monitor.binding_ip=`echo $local_ip`/" __agent.conf
-    echo linux replace ip: $local_ip
-elif ( echo ${os} |grep -q "Darwin" );then
-    local_ip=`/sbin/ifconfig en0|grep cast|awk '{print $2}'|awk -F ":" '{print $1}'`
-    sed -i "" "s/.*monitor.binding_ip=.*/monitor.binding_ip=`echo $local_ip`/" __agent.conf
-    echo mac replace ip: $local_ip
-fi
-
 java -server -cp "lib/*" org.ngrinder.NGrinderAgentStarter --mode monitor --command run $@
