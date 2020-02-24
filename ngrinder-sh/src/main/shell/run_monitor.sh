672b6265d7a0422bf65bca63a083d8f4c6fe5432
lingjian <lingjian@qudian.com>
2020-02-24 18:27:16 +0800

    3.4.1 修改pom.xml 修改代理、监控脚本

--- ngrinder-sh/src/main/shell/run_monitor.sh
+++ ngrinder-sh/src/main/shell/run_monitor.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 curpath=`dirname $0`
 cd ${curpath}
-local_ip=`ifconfig|grep Bcast|head -n 1|awk '{print $2}'|awk -F ":" '{print $2}'`
-sed -i "3c monitor.binding_ip=`echo $local_ip`" __agent.conf
 java -server -cp "lib/*" org.ngrinder.NGrinderAgentStarter --mode monitor --command run $@
